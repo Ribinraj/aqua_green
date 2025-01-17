ac6d53469fde0f2ff2b1ecd1acde4545eef6795f
Ribinraj <ribinrajop@gmail.com>
2025-01-17 15:10:16 +0530

    endpoint changed

--- lib/core/urls.dart
+++ lib/core/urls.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static const baseUrl = 'https://thefairy.in/rogreen/api/';
+  static const baseUrl = 'https://app.myaquagreen.com/api/';
   static const login = 'login';
   static const sendOtp = 'login/forgot';
   static const resendOtp = 'login/resend';
